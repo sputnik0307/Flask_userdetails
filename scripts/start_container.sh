314d056fe15af7676756e827922f24545e9468ee
Nikhil Bhalerao <81256273+sputnik0307@users.noreply.github.com>
2024-04-24 08:38:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 #pull the docker image from docker hub
-docker pull sputnik03/simple-python-flask-app
+docker pull sputnik03/user-details-app-codebuild
 
 #run the docker image as a container
-docker run -d -p 5000:5000 sputnik03/simple-python-flask-app
+docker run -d -p 3000:3000 sputnik03/user-details-app-codebuild
 
